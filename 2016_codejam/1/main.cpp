98dc3ac60908d67b49f4e36be0fddc3bb5423fdc
jjaem.kim <mtmt8282@naver.com>
2016-06-25 14:11:44 +0900

    Add 2016 LGE Code Jam Prj

---
+++ 2016_codejam/1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class node
+{
+	double parent;
+	double child;
+	double cnt;
+
+	double find_parent();
+	double find_child();
+};
+
+int main()
+{
+	cout<< "test"<< endl;
+	return 0;
+}
