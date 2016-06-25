245c3bf56e009cc7b53cb46845d799c0f40dccf8
jjaem.kim <mtmt8282@naver.com>
2016-06-25 17:06:40 +0900

    Modfiy LGE code jam #1

--- 2016_codejam/1/main.cpp
+++ 2016_codejam/1/main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class node
+class Node
 {
-	double parent;
-	double child;
-	double cnt;
+	private:
+		static vector<Node*> _node_list;
+		double _parent;
+		double _child;
 
-	double find_parent();
-	double find_child();
+	public:
+		Node (double p, double c) : _parent(p), _child(c)
+		{
+			_node_list.push_back(this);
+		}
+		double find_parent(vector<Node*> _list);
+		double find_child(vector<Node*> _list);
 };
+vector<Node*> Node::_node_list;
 
 int main()
 {
-	cout<< "test"<< endl;
+	double case_cnt = 0;
+	double people_cnt = 0;
+	double tmp_p = 0;
+	double tmp_c = 0;
+
+	cin>> case_cnt;
+	while(case_cnt--)
+	{
+		cin>> people_cnt;
+		while(people_cnt--)
+		{
+			cin>> tmp_p;
+			cin>> tmp_c;
+
+			Node* tmp_node = new Node(tmp_p, tmp_c);
+		}
+	}
+
 	return 0;
 }
